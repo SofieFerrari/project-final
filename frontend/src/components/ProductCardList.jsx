e8a68fc2c4b72fc659464cdffc7f8af900816b2d
SofieFerrari <sofiestrahl@gmail.com>
2024-06-05 14:36:53 +0200

    created structure for pcl component

---
+++ frontend/src/components/ProductCardList.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+// import { ProductCard } from ./components/ProductCard
+
+export const ProductCardList = () => {
+  const [listItem, setListItem] = useState([])
+
+  //här vet jag inte riktigt, men det borde vara någon form av event?
+  //plus det borde vara någon event prevent default?
+  const handleClick = (e) => {
+    setListItem = handleClick
+  }
+
+  return (
+    
+  )
+}
